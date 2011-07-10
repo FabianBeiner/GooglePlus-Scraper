e5b2e1b2fd21051d948211185d0796fadd0fef55
Fabian Beiner <mail@fabian-beiner.de>
2011-07-10 20:26:50 +0200

    Updated Frank's examples.

--- feed.example.php
+++ feed.example.php
@@ -7,48 +7,40 @@
  * @license    Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Germany
  *             (http://creativecommons.org/licenses/by-nc-sa/3.0/de/deed.en)
  * @link       http://bueltge.de
- * @version    1.0
- * @date       04.07.2011 17:17:29
+ * @version    1.1
+ * @date       10.07.2011 20:11:01
 */
 
-require_once( 'rss2.class.php' );
-require_once( 'googleplus.class.php' );
-$arrTests = array( 'https://plus.google.com/111291152590065605567/posts' );
+require_once 'rss2.class.php';
+require_once 'googleplus.class.php';
 
-foreach ( $arrTests as $strTest ) {
-	$oGooglePlus = new GooglePlus( $strTest );
-	if ( $oGooglePlus -> isReady ) {
+$oGooglePlus = new GooglePlus('https://plus.google.com/111291152590065605567/posts');
+if ($oGooglePlus->isReady) {
 
-		$channel = array();
-		$channel['title'] = 'Google+ profile of ' . $oGooglePlus -> getName();
-		$channel['description'] = $oGooglePlus -> getIntroduction() . ', ' . $oGooglePlus -> getOccupation();
-		$channel['link'] = 'https://plus.google.com/' . $oGooglePlus -> strProfileId . '/posts';
+	$channel                = array();
+	$channel['title']       = 'Google+ profile of ' . $oGooglePlus->get('name');
+	$channel['description'] = strip_tags($oGooglePlus->get('introduction'));
+	$channel['link']        = $oGooglePlus->get('url');
 
-		$arrPosts = $oGooglePlus -> getPosts();
-		$arrLinks = $oGooglePlus -> getLinks();
-		$i = 0;
-		$w = new Rss(
-			'php://output',
-			$channel['title'],
-			$channel['description'],
-			$channel['link'],
-			date('Y-m-d')
-		);
-		foreach ($arrPosts as $strPost) {
+	$w = new Rss(
+		'php://output',
+		$channel['title'],
+		$channel['description'],
+		$channel['link'],
+		date('Y-m-d')
+	);
+	if ($oGooglePlus->get('plainposts') != 'n/A') {
+		foreach ($oGooglePlus->get('plainposts') as $arrLink) {
 			$item = array();
-			$item['title']       = $oGooglePlus -> getShortText( $strPost, 120, TRUE );
-			if ( $arrLinks[$i] )
-				$item['link']    = 'https://plus.google.com/' . $oGooglePlus -> strProfileId . '/posts/' . $arrLinks[$i];
-			$item['description'] = $strPost;
-			if ( $arrLinks[$i] )
-				$item['guid']    = 'https://plus.google.com/' . $oGooglePlus -> strProfileId . '/posts/' . $arrLinks[$i];
-			//$item['date'] = date('Y-m-d');
-
+			$item['title']       = GooglePlus::getShortText($arrLink[0], 120, true);
+			if ($arrLink[1]) {
+				$item['link']    = $arrLink[1];
+				$item['guid']    = $arrLink[1];
+			}
+			$item['description'] = $arrLink[0];
 			$w->add_item($item);
-			$i++;
 		}
-		$i++;
-
-		$w->end_rss();
 	}
+
+	$w->end_rss();
 }
--- googleplus_feed.class.php
+++ googleplus_feed.class.php
@@ -7,74 +7,56 @@
  * @license    Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Germany
  *             (http://creativecommons.org/licenses/by-nc-sa/3.0/de/deed.en)
  * @link       http://bueltge.de
- * @version    1.0
- * @date       04.07.2011 17:17:29
+ * @version    1.1
+ * @date       10.07.2011 20:18:01
 */
 
-require_once( 'googleplus.class.php' );
-$arrTests = array( 'https://plus.google.com/111291152590065605567/posts' );
+require_once 'googleplus.class.php';
 
-@date_default_timezone_set( 'GMT' );
+@date_default_timezone_set('GMT');
 
-foreach ( $arrTests as $strTest ) {
-	$oGooglePlus = new GooglePlus( $strTest );
-	
-	if ( $oGooglePlus -> isReady ) {
-		
+	$oGooglePlus = new GooglePlus('https://plus.google.com/111291152590065605567/posts');
+
+	if ($oGooglePlus->isReady) {
+
 		$writer = new XMLWriter();
-		$writer -> openURI( 'php://output' );
-		$writer -> startDocument( '1.0', 'UTF-8' );
-		$writer -> setIndent( TRUE );
-		
+		$writer->openURI('php://output');
+		$writer->startDocument('1.0', 'UTF-8');
+		$writer->setIndent(true);
+
 		// declarer for rss element
-		$writer -> startElement( 'rss' );
-		$writer -> writeAttribute( 'version', '2.0' );
-		$writer -> writeAttribute( 'xmlns:atom', 'http://www.w3.org/2005/Atom' );
-		
-			// start element channel
-			$writer -> startElement( 'channel' );
-				//$writer -> writeElement( 'ttl', '0' );
-				$writer -> writeElement( 'title', 'Google+ profile of ' . $oGooglePlus -> getName() );
-				$writer -> writeElement( 'description', $oGooglePlus -> getIntroduction() . ', ' . $oGooglePlus -> getOccupation() );
-				$writer -> writeElement( 'link', 'https://plus.google.com/' . $oGooglePlus -> strProfileId . '/posts' );
-				$writer -> writeElement( 'pubDate', date( 'D, d M Y H:i:s e' ) );
-				/*
-				$writer -> startElement( 'image' );
-				$writer -> writeElement( 'title', 'Latest Products' );
-				$writer -> writeElement( 'link', 'http://www.domain.com/link.htm' );
-				$writer -> writeElement( 'url', 'http://www.iab.net/media/image/120x60.gif' );
-				$writer -> writeElement( 'width', '120' );
-				$writer -> writeElement( 'height', '60' );
-				*/
-			$writer -> endElement();
-			// end element channel
-			
-				$arrPosts = $oGooglePlus -> getPosts();
-				$arrLinks = $oGooglePlus -> getLinks();
-				$i = 0;
-				foreach ($arrPosts as $strPost) {
-					$writer -> startElement( 'item' );
-					$writer -> writeElement( 'title', $strPost );
-					// Found a link?
-					if ( $arrLinks[$i] )
-						$writer -> writeElement( 'link', 'https://plus.google.com/' . $oGooglePlus -> strProfileId . '/posts/' . $arrLinks[$i] );
-					//$writer -> writeElement( 'description', '' );
-					if ( $arrLinks[$i] )
-						$writer -> writeElement( 'guid', 'https://plus.google.com/' . $oGooglePlus -> strProfileId . '/posts/' . $arrLinks[$i] );
-					//$writer -> writeElement( 'pubDate', '' );
-					
-					$writer -> endElement();
-					$i++;
+		$writer->startElement('rss');
+		$writer->writeAttribute('version', '2.0');
+		$writer->writeAttribute('xmlns:atom', 'http://www.w3.org/2005/Atom');
+
+		// start element channel
+		$writer->startElement('channel');
+		$writer->writeElement('title', 'Google+ profile of ' . $oGooglePlus->get('name'));
+		$writer->writeElement('description', strip_tags($oGooglePlus->get('introduction')));
+		$writer->writeElement('link', $oGooglePlus->get('url'));
+		$writer->writeElement('pubDate', date('D, d M Y H:i:s e') );
+		$writer->endElement();
+		// end element channel
+
+		if ($oGooglePlus->get('plainposts') != 'n/A') {
+			foreach ($oGooglePlus->get('plainposts') as $arrLink) {
+				$writer->startElement('item');
+				$writer->writeElement('title', GooglePlus::getShortText($arrLink[0], 120, true));
+				$writer->writeElement('description', $arrLink[0]);
+				if ($arrLink[1]) {
+					$writer->writeElement('link', $arrLink[1]);
+					$writer->writeElement('guid', $item['guid']    = $arrLink[1]);
 				}
-				$i++;
-			
-			// end element channel
-			$writer -> endElement();
-			
-		// End rss element
-		$writer -> endElement();
-		$writer -> endDocument();
-		
-		$writer -> flush();
+				$writer->endElement();
+			}
+		}
+
+	// end element channel
+	$writer->endElement();
+
+	// End rss element
+	$writer->endElement();
+	$writer->endDocument();
+
+	$writer->flush();
 	}
-}
