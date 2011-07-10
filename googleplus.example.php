0c6b6e3856a46281d1deccf11334cf3de98c1e55
Fabian Beiner <mail@fabian-beiner.de>
2011-07-10 20:10:06 +0200

    Updated examples.

--- googleplus.example.php
+++ googleplus.example.php
@@ -1,3 +1,6 @@
+<?php
+    header('Content-Type: text/html; charset=utf-8');
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,7 +10,7 @@
     body {
       background-color:#ebebeb;
       color:#111;
-      font-family:Corbel, "Lucida Grande", "Lucida Sans Unicode",  "Lucida Sans", "DejaVu Sans", "Bitstream Vera Sans", "Liberation Sans", Verdana, sans-serif;
+      font-family:Corbel, "Lucida Grande", "Lucida Sans Unicode", "Lucida Sans", "DejaVu Sans", "Bitstream Vera Sans", "Liberation Sans", Verdana, sans-serif;
       font-size:14px;
       margin:20px auto;
       width:700px;
@@ -34,24 +37,32 @@
         $oGooglePlus = new GooglePlus($strTest);
 
         if ($oGooglePlus->isReady) {
-            echo '<h1>Parsing Google+ profile of “' . $oGooglePlus->getName() . '”</h1>';
-            echo '<p>Profile: <a href="https://plus.google.com/' . $oGooglePlus->strProfileId . '/posts">https://plus.google.com/' . $oGooglePlus->strProfileId . '/posts</a><b></b><br>Introduction: <b>' .  $oGooglePlus->getIntroduction() . '</b><br>Occupation: <b>' . $oGooglePlus->getOccupation() . '</b></p><hr>';
-            $arrPosts = $oGooglePlus->getPosts();
-            $arrLinks = $oGooglePlus->getLinks();
-            $i        = 0;
-            foreach ($arrPosts as $strPost) {
-                // Found a link?
-                if ($arrLinks[$i]) {
-                    echo '<a href="https://plus.google.com/' . $oGooglePlus->strProfileId . '/posts/' . $arrLinks[$i] . '">' . $strPost . '</a><hr>';
-                }
-                else {
-                    echo $strPost . '<hr>';
-                }
-                $i++;
+            echo '<h1>Parsing Google+ profile of “' . $oGooglePlus->get('name') . '”</h1>';
+            if (($strDesc = $oGooglePlus->get('description')) && ($strDesc != 'n/A')) {
+                echo '<h2>' . $strDesc . '</h2>';
             }
-            $i++;
+            echo '<p><img src="' . $oGooglePlus->get('image') . '" width="100" height="100" alt="Profile Image" style="float:right;"></p>';
+            echo '<p><b>Google+ id:</b> ' . $oGooglePlus->get('id') . '</p>';
+            echo '<p><b>Google+ profile url:</b> <a href="' . $oGooglePlus->get('url') . '">' . $oGooglePlus->get('url') . '</a></p>';
+            echo '<p><b>First name:</b> ' . $oGooglePlus->get('firstname') . ' <b>Last name:</b> ' . $oGooglePlus->get('lastname') . ' <b>Nickname:</b> ' . $oGooglePlus->get('nickname') . ' <b>Other names:</b> ' . $oGooglePlus->get('othernames') . '</p>';
+            echo '<p><b>Occupation:</b> ' . $oGooglePlus->get('occupation') . '</p>';
+            echo '<p><b>Introduction:</b> ' . $oGooglePlus->get('introduction') . '</p>';
+            echo '<p><b>Links:</b> ';
+            if ($oGooglePlus->get('links') == 'n/A') {
+                echo 'n/A';
+            } else {
+                foreach ($oGooglePlus->get('links') as $arrLink) {
+                    echo '<a href="' . $arrLink[1] . '">' . $arrLink[0] . '</a> ';
+                }
+            }
+            echo '</p>';
+            echo '<p><b>Posts as Plain (max. 200 chars):</b></p>';
+            if ($oGooglePlus->get('plainposts') != 'n/A') {
+                foreach ($oGooglePlus->get('plainposts') as $arrLink) {
+                    echo '<p>' . GooglePlus::getShortText($arrLink[0], 200, true) . ' <a href="' . $arrLink[1] . '">Read full post!</a></p>';
+                }
+            }
+            echo '<hr style="clear:right;">';
         }
     }
 ?>
-</body>
-</html>
